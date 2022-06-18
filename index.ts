bbf36598deb690b89b2a051b109f25e5c2f22523
Kumar Adhikari <kumarad556@gmail.com>
2022-06-18 22:30:38 +0545

    setup changes

--- index.ts
+++ index.ts
@@ -1,15 +1,31 @@
 import express from 'express';
-// const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import exampleRoute from './src/routes/exampleRoute';
+
+//env
 dotenv.config();
 
+//import routes
+import exampleRoute from './src/routes/exampleRoute';
+
+//initialize app
 const app = express();
+
+//db
+const mongo_uri = process.env.MONGO_URI_LOCAL || 'mongodb://localhost:27017/test';
+mongoose
+	.connect(mongo_uri)
+	.then(() => console.log('===== MONGODB CONNECTED ====='))
+	.catch(() => console.log('Failed to connect to MONGODB!!!'));
+
+//middlewares
+app.use(express.json());
 app.use(cors());
 
-app.use('/example', exampleRoute);
+//Routes
+app.use('/api/v1/example', exampleRoute);
 
+//start server
 const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`===== SERVER RUNNING ON PORT: ${port} =====`));
